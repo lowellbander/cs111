84bcd4211cb5bbf2dbe1fdf89ab7ef1e7145e68f
Nicole Yee <xosilverliningox@gmail.com>
2014-07-02 02:43:46 -0700

    fixed null termination of string for strlen

--- lab1-Skeleton/read-command.c
+++ lab1-Skeleton/read-command.c
@@ -1,5 +1,20 @@
 // UCLA CS 111 Lab 1 command reading
 
+
+/* TODO:comments
+        syntax checker
+        make_command_stream --> recognize separate commands 
+        deal with new line counts, passing it back up tree for syntax errors
+        read_command --> trivial, but make commands a linked list
+        
+*/ 
+
+
+/*For Nicole: change make_command's arguments to include line number
+              command stream
+  
+*/
+
 #include "command.h"
 #include "command-internals.h"
 #include "alloc.h"
@@ -18,7 +33,7 @@ struct command_stream
 
 /* Checks to see that a character in the input stream is valid */
 void validate(char c) {
-  //TODO: finish implementation
+  //TODO: finish implementation and add argument for line number
   if (isalpha(c) || 
       isdigit(c) ||
       c == '!' ||
@@ -37,13 +52,15 @@ void validate(char c) {
       c == '|' ||
       c == ';' ||
       c == '&' ||
+      c == '(' ||
+      c == ')' ||
+      c == ' ' ||
       c == '\n' ||
-      c == ' ')
+      c == '\t')
       return;
     else
     {
-      printf("invalid char: %c", c);
-      error(1, 0, "invalid character");
+      error(1, 0, "invalid character: %c", c);
       return;
     }
 };
@@ -54,26 +71,30 @@ void validate(char c) {
 char* buildString(int (*get_next_byte) (void *),
 		     void *get_next_byte_argument)
 {
-  const int INC = 10;
-  size_t size = INC;
-  char* string = calloc(size, sizeof(char));
+  size_t size = 8;
+  char* string = checked_malloc(8*sizeof(char));
   int i = 0;
+  int line = 1;
 
   char c  = get_next_byte(get_next_byte_argument);
   validate(c);
 
+  if(c == '\n')
+    ++line;
   while (c != EOF)
     {
       validate(c);
 
       if (strlen(string) >= size) 
         string = checked_grow_alloc(string, &size);
-
       string[i] = c;
       c  = get_next_byte(get_next_byte_argument);
+      if(c == '\n')
+        ++line;
       ++i;
     }
-
+  //Null terminate string
+  string[i] = '\0';
   return string;
 }
 
@@ -104,6 +125,7 @@ char* get_opt_ptr(char* beg, char* end)
   ptr = end;
   // end syntax checking
 
+  // Scan for sequence command
   while(ptr != beg)
   {
     if (*ptr == ';')
@@ -111,6 +133,7 @@ char* get_opt_ptr(char* beg, char* end)
     --ptr;
   }
   ptr = end;
+  // Scan for subshell command
   while(ptr != beg)
   {
     if (*ptr == ')')
@@ -118,11 +141,12 @@ char* get_opt_ptr(char* beg, char* end)
     --ptr;
   }
   ptr = end;
+  // Scan for pipe command
   while(ptr != beg)
   { 
     if (*ptr == '|')
     {
-      //Check if OR
+      //Check if pipe
       if(*(ptr-1) != '|')
         goto done;
       ptr--;
@@ -130,6 +154,7 @@ char* get_opt_ptr(char* beg, char* end)
     --ptr;
   }
   ptr = end;
+  // Scan for AND command
   while(ptr != beg)
   {
     if (*ptr == '&')
@@ -137,6 +162,7 @@ char* get_opt_ptr(char* beg, char* end)
     --ptr;
   }
   ptr = end;
+  // Scan for OR command
   while(ptr != beg)
   {
     if (*ptr == '|')
@@ -163,7 +189,7 @@ make_command (char* beg, char* end)
    * (lowest)
    * */
 
-  //Check command type
+  // Check command type and makes command, calling function recursively if needed
   if (optPtr == beg)
   {
     // check to see that operators have operands
@@ -174,9 +200,44 @@ make_command (char* beg, char* end)
     char* word = malloc(sizeof(char)*(end-beg));
     char* ptr = beg;
     int i = 0;
-    for (; ptr != end + 1; ++ptr, ++i) 
-    {
-      word[i] = *ptr;
+    int line = 0;
+    bool foundSpace = false;
+
+    for (; ptr != end; ++ptr) 
+    {  
+      printf("i: %d, c: <%c>\n", i, *ptr);
+      // Check if newline
+      if (*ptr == '\n')
+      {
+        ++line;
+        foundSpace = false;
+      }
+      else
+      {
+        // Skip over extra white spaces and tabs
+        if (!foundSpace && *ptr != '\t')
+        {
+          //printf("Writing1\n");
+          word[i] = *ptr;
+          if (*ptr == ' ')
+            foundSpace = true;
+          else
+            foundSpace = false;
+          ++i;
+        }
+        else
+        {
+          if (*ptr != ' ' && *ptr != '\t')
+          {
+            //printf("Writing2\n");
+            word[i] = *ptr;
+            ++i;
+            foundSpace = false;
+          }
+          else if (*ptr != '\t')
+            foundSpace = true;
+        }
+      }
     }
     *(com->u.word) = word;
   }
@@ -221,8 +282,8 @@ make_command_stream (int (*get_next_byte) (void *),
 {
 
   char* string = buildString(get_next_byte, get_next_byte_argument);
-  char* end = string + strlen(string) - 2;
-
+  char* end = string + strlen(string);
+  printf("strlen: %d  end: <%c>\n", strlen(string), *end);
   command_t com = make_command(string, end);
 
   command_stream_t stream = checked_malloc(sizeof(struct command_stream));
