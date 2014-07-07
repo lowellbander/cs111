53a6e6e6c741c529329a2b1d6a63271cbaa66b28
Lowell Bander <lowellbander@gmail.com>
2014-07-07 11:27:48 -0700

    successfully executes simple system call

--- lab1-Skeleton/execute-command.c
+++ lab1-Skeleton/execute-command.c
@@ -2,6 +2,7 @@
 
 #include "command.h"
 #include "command-internals.h"
+#include <stdlib.h>
 
 #include <error.h>
 
@@ -20,5 +21,9 @@ execute_command (command_t c, int time_travel)
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
+
+  // traverse the command tree and execute commands with system()
+  system("echo hello, world!");
+
   error (1, 0, "command execution not yet implemented");
 }
