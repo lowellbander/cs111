e0b1430c542e98192380fcc2a24a691748f186dd
Nicole Yee <xosilverliningox@gmail.com>
2014-07-22 22:30:11 -0700

    implemented get_dependcy files

--- lab1-Skeleton/execute-command.c
+++ lab1-Skeleton/execute-command.c
@@ -11,6 +11,101 @@
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
+   
+typedef struct file_stream *file_stream_t;
+typedef struct file_node *file_node_t;
+
+struct file_stream
+{ 
+  file_node_t head;
+  file_node_t tail;
+  file_node_t curr;
+};
+
+struct file_node
+{
+  char* name;
+  file_node_t next;
+};						
+
+// Adds a file_node to the file_stream
+
+void push_file (file_stream_t stream, char* file)
+{
+  file_node_t node = checked_malloc(sizeof(struct file_node));
+  node->name = checked_malloc(sizeof(file));
+  *(node->name) = *file;
+  node->next = NULL;
+
+  if (stream->head == NULL)
+  {
+    //printf("Starting stream\n");
+    //puts(file);
+    // the stream is empty 
+    stream->head = node;
+    stream->tail = node;
+    stream->curr = node;
+    return;
+  }
+  else
+  {
+    //printf("pushing file\n");
+    //puts(file);
+    stream->tail->next = node;
+    stream->tail = stream->tail->next;
+    return;
+  }
+}
+
+// Retrieves all of the file dependencies and stores them in file stream
+// Goes through recursively until looking at the input/output of each
+//    simple command
+
+file_stream_t
+get_file_depends (command_t c)
+{
+  file_stream_t depends = checked_malloc(sizeof(struct file_stream));
+  // Complete command is just a simple command
+  switch (c->type)
+  {
+    case SIMPLE_COMMAND:
+    {
+      //printf("depending on simple\n");
+      if (c->input != NULL)
+        push_file(depends, c->input);
+      if (c->output != NULL)
+        push_file(depends, c->output);
+      break;
+    }
+    case SUBSHELL_COMMAND:
+    {
+      depends = get_file_depends (c->u.subshell_command);
+      break;
+    }
+    case AND_COMMAND:
+    case OR_COMMAND:
+    case SEQUENCE_COMMAND:
+    case PIPE_COMMAND:
+    {
+      //printf("depending on pipe\n");
+      // Set depends to left dependency list
+      depends = get_file_depends (c->u.command[0]);
+      // Append right depedency list to tail of current list
+      depends->tail->next = (get_file_depends (c->u.command[1]))->head;
+      
+      // Update tail
+      depends->curr = depends->tail->next;
+      while(depends->curr->next != NULL)
+      {
+        depends->curr = depends->curr->next;
+      }
+      depends->tail = depends->curr;
+      break;
+    }
+  }
+    
+  return depends;
+}
 
 int
 command_status (command_t c)
@@ -18,7 +113,8 @@ command_status (command_t c)
   return c->status;
 }
 
-int find_command_size (command_t c)
+int
+find_command_size (command_t c)
 {
   int len = 0;
   switch (c->type)
@@ -52,7 +148,8 @@ int find_command_size (command_t c)
   return len;
 }
 
-char* build_sys_string (command_t c)
+char*
+build_sys_string (command_t c)
 {
   int len = find_command_size(c);
   char* word = checked_malloc(len);
@@ -120,11 +217,11 @@ execute_command (command_t c, int time_travel)
     system(build_sys_string(c));
 }
 
-void* hello(void* void_ptr)
-{
-  printf("hello, world!\n");
-  return NULL;
-}
+//void* hello(void* void_ptr)
+//{
+//  printf("hello, world!\n");
+//  return NULL;
+//}
 
 void
 exe_stream (command_stream_t stream, int time_travel)
@@ -140,9 +237,20 @@ exe_stream (command_stream_t stream, int time_travel)
     //group together the commands by dependencies, possible into command streams
     //then execute each command stream in it's own thread
 
-    pthread_t t1;
-    pthread_create(&t1, NULL, &hello, NULL);
-    pthread_join(t1, NULL);
+    //pthread_t t1;
+    //pthread_create(&t1, NULL, &hello, NULL);
+    //pthread_join(t1, NULL);
+    
+    file_stream_t files = checked_malloc(sizeof(struct file_stream));
+    files = get_file_depends(read_command_stream(stream));
+    files->curr = files->head;
+    
+    printf("Depends on:\n");
+    while (files->curr != NULL)
+    {
+      printf("[%s]\n", files->curr->name);
+      files->curr = files->curr->next;
+    }
     error(1, 0, "time travel not yet implemented\n");
   }
 }
